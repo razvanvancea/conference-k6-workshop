1e592a59f8643083d8d389d05625be9043a343f4
Razvan Vancea <razvanvancea94@gmail.com>
2026-06-02 17:17:03 +0300

    update tests

--- scripts/5-env-vars.test.js
+++ scripts/5-env-vars.test.js
@@ -1,6 +1,6 @@
 /**
 5.1 refactor BASE_URL constant to read the value from an environment variable with a fallback to 'http://localhost:3001'
-Note: run the test from CLI passing the URL environment variable (e.g. k6 run -e URL=http://localhost:3001 4-env-vars.test.js)
+Note: run the test from CLI passing the URL environment variable (e.g. k6 run -e URL=http://localhost:3001 scripts/5-env-vars.test.js)
 */
 
 import http from 'k6/http';
