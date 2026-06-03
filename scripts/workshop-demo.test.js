00385a2dd42e957c07d5d0976a5f4f6906bf1252
RV <razvanvancea94@gmail.com>
2026-06-04 00:18:30 +0300

    Update workshop-demo.test.js

--- scripts/workshop-demo.test.js
+++ scripts/workshop-demo.test.js
@@ -6,6 +6,7 @@ import { check, sleep } from 'k6';
 import { randomSleep } from '../utils/random-utils.js';
 import { errorHandler } from '../utils/error-handler.js';
 import { debugResponse } from '../utils/utils.js';
+import { expect } from 'https://jslib.k6.io/k6-testing/0.6.1/index.js';
 
 const BASE_URL = "http://localhost:3001";
 
